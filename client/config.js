a36c89237b8eb00fd315c5ee11e4f77e0f502555
David Wolgemuth <davidjwolgemuth@gmail.com>
2017-02-16 11:43:58 -0600

    Connected all files on client side

--- client/config.js
+++ client/config.js
@@ -2,3 +2,16 @@
     /client/config.js
     Configure client routes, and really anything
 */
+var app = angular.module("itemsApp", ["ngRoute"]);
+console.log("Creating App");
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "partials/items-index.html",
+        controller: "itemsIndex"
+    }).when("/new-item", {
+        templateUrl: "partials/new-item.html",
+        controller: "newItem"
+    });
+    console.log("Configuring Routes")
+});
--- client/factories/item.js
+++ client/factories/item.js
@@ -2,3 +2,12 @@
     /client/factories/item.js
     The "Client Side DB" / connection between the client and the server (where the client can request additional data)
 */
+app.factory("itemFactory", function () {
+    
+    console.log("Created Item Factory");
+
+    var factory = {};
+
+    return factory;
+
+});
